a184d50c266207396eee8505f0dacc91fc537979
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-02-21 17:19:35 +0100

    ajout de code dans la classe Chat

--- Animal/src/com/sdz/classesabstraitesetlesinterfaces/Chat.java
+++ Animal/src/com/sdz/classesabstraitesetlesinterfaces/Chat.java
@@ -1,4 +1,19 @@
 package com.sdz.classesabstraitesetlesinterfaces;
 
-public class Chat {
+public class Chat extends Felin{
+
+    public Chat (){
+
+    }
+
+    public Chat (String couleur, int poids){
+        this.couleur=couleur;
+        this.poids=poids;
+    }
+
+    void crier(){
+        System.out.println("Je miaule sur les toits");
+    }
+
+
 }
