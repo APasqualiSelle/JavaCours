1ffb98a11521b517e77783f29c4a2d57d3c30ab5
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-07 23:33:09 +0100

    ajout d'exercises d'Arrays

---
+++ ArraysLoiane/src/Exercicio4.java
@@ -0,0 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void main(String[] args){
+Scanner scan = new Scanner(System.in);
+        int[] vetorA = new int [15];
+        double[] vetorB = new double[vetorA.length];
+
+        for(int i =0;i < vetorA.length;i++){
+            System.out.println("Entre com o valor da posição "+ i);
+            vetorA[i]=scan.nextInt();
+            vetorB[i]=Math.sqrt(vetorA[i]);
+            }
+
+            DecimalFormat df = new DecimalFormat("###,###.##");
+
+        System.out.println();
+        System.out.print("Vetor A = ");
+        for(int i =0;i<vetorA.length;i++){
+            System.out.print(vetorA[i]+ " ");
+        }
+        System.out.println();
+        System.out.print("Vetor B = ");
+        for(int i=0;i<vetorA.length;i++){
+            System.out.print(df.format(vetorB[i])+ " ");
+        }
+
+    }
+}
