3999d0dc8afb75c9b710b84ce6149210495a77fd
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-09 12:17:28 +0100

    ajout de code solution Encapsulation Tim

---
+++ TimEncapsulationCorrection/src/com/alineselle/Main.java
@@ -0,0 +1,16 @@
+package com.alineselle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Printer printer = new Printer(50,false);
+        System.out.println("Initial page count = "+printer.getPagesPrinted());
+       int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was "+pagesPrinted+" new total print count for printer = "+printer.getPagesPrinted());
+        pagesPrinted= printer.printPages(2);
+        System.out.println("Pages printed was "+ pagesPrinted+" new total print count for printer = "+printer.getPagesPrinted());
+        System.out.println(printer.addToner(30));
+        System.out.println(printer.addToner(10));
+    }
+}
