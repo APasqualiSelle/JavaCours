ad0e842edca75702bd7696dc7ae687182cf06b19
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-09 20:12:25 +0100

    ajout de nouveaux exercices de Loiane, arrays

---
+++ ArraysLoiane/src/Palindrome.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Palindrome {
+
+    public static void main(String[] args){
+
+        Scanner clavier = new Scanner(System.in);
+    int[] tableau = {1,2};
+    for(int i = 0; i<tableau.length;i++){
+        System.out.println("O valor da posição "+i+" do tableau = "+tableau[i]);
+
+    }
+
+
+
+
+
+    }
+}
