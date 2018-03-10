4e480d5f2fc5a4d71ee754088b62e8d82040b835
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-10 22:23:27 +0100

    ajout d'exercice 15.1 array

---
+++ ArraysLoiane/src/Exercicio15_1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Exercicio15_1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+        for (int i = 0; i < vetorA.length; i++) {
+
+            System.out.println("Entre com a posição do vetorA " + i);
+            vetorA[i] = scan.nextInt();
+
+        }
+        int par = 0;
+        int impar = 0;
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            if (vetorA[i] % 2 == 0) {
+
+                par++;
+
+            } else {
+
+                impar++;
+            }
+        }
+
+        int pourcentageP =  (100*par)/vetorA.length;
+        int pourcentageI = (100*impar)/vetorA.length;
+
+
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+
+
+        }
+
+        System.out.println();
+
+        System.out.println(pourcentageP+" % dos elementos do vetor A contém númeoros pares.");
+        System.out.println(pourcentageI+" % dos elementos do vetor A contém númeoros ímpares.");
+
+
+    }
+}
