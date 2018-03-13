cd43180cfafd7ac422b853f28648b5c36aee8c0d
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-13 17:49:25 +0100

    ajout des exercices 16 et 17

---
+++ ArraysLoiane/src/Exercicio17.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Exercicio17 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] vetorA = new int[10];
+
+        for (int i = 0; i < vetorA.length; i++) {
+
+            System.out.println("Entre a idade(valor armazenado) de posição " + i);
+            vetorA[i] = scan.nextInt();
+        }
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+
+
+        int qtePessoasMaior15 = 0;
+        for (int i = 0; i < vetorA.length; i++) {
+            if (vetorA[i] > 35) {
+
+                qtePessoasMaior15++;
+            }
+
+            }
+
+        System.out.println();
+        System.out.println("A quantidade de pessoas com idade superior a 35 é igual a "+qtePessoasMaior15);
+    }
+}
--- ArraysLoiane/src/Palindrome.java
+++ ArraysLoiane/src/Palindrome.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 public class Palindrome {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner clavier = new Scanner(System.in);
-    int[] tableau = {1,2};
-    for(int i = 0; i<tableau.length;i++){
-        System.out.println("O valor da posição "+i+" do tableau = "+tableau[i]);
 
+        System.out.println("Digite uma palavra : ");
+
+
+       String palavra = clavier.nextLine();
+        String[] tableau = {palavra};
+        System.out.println(palavra.length());
+       for(int i=0;i<tableau.length;i++){
+           System.out.println(tableau[i]);
+
+
+       }
+
+
+            //como entrar elementos indefinidos num array?
+
+        }
     }
 
-
-
-
-
-    }
-}
