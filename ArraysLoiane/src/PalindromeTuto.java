3b3f1293944e09bd5c76423d4004973ff50fa906
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-16 18:55:34 +0100

    Ajout du exercice Palindrome du Tutorial https://www.youtube.com/watch?v=vT_dDd3HCvA

---
+++ ArraysLoiane/src/PalindromeTuto.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class PalindromeTuto {
+
+    public static void main(String[] args) {
+        System.out.println("Enter with a word : ");
+
+        Scanner clavier = new Scanner(System.in);
+        char reponse = ' ';
+
+        boolean isPal = true;
+        do {
+
+            String pal = clavier.nextLine();
+            int startPos = 0;
+            int endPos = pal.length() - 1;
+
+
+            while (startPos < endPos) {
+
+                //substring est une methode pour vérifier la positions de caracteres d'une String
+                String startLetter = pal.substring(startPos, startPos + 1);
+                String endLetter = pal.substring(endPos, endPos + 1);
+
+                if (startLetter.equals(endLetter) == false) {
+
+                    isPal = false;
+                    break;
+                }
+                startPos++;
+                endPos--;
+            }
+
+            if (isPal == true) {
+                System.out.println(pal + " is a Palindrome!");
+            } else {
+                System.out.println(pal + " is NOT a Palindrome");
+            }
+            do {
+                System.out.println("Would you like to try again?(O/N)");
+
+                reponse = clavier.nextLine().charAt(0);
+
+            } while (reponse != 'O' && reponse != 'N');
+            System.out.println("Enter another word: ");
+        }
+
+        while (reponse == 'O');
+        System.out.println("Good bye!");
+    }
+
+    }
+
+
