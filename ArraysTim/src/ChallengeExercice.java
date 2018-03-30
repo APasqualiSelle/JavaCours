d452bb28b60b8929e87e410c511a95752cbeaf68
APasqualiSelle <33148018+APasqualiSelle@users.noreply.github.com>
2018-03-30 21:09:01 +0200

    ajout de nouvelles lignes de code pour le challenge

--- ArraysTim/src/ChallengeExercice.java
+++ ArraysTim/src/ChallengeExercice.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 public class ChallengeExercice {
-
+private static Scanner scanner = new Scanner(System.in);
     public static void main(String[]args){
 
         //Create a program using arrays that sorts a list of integers descending order.
@@ -13,6 +14,38 @@ public class ChallengeExercice {
         //and sortIntegers should sort the array and return a new array containing the sorted numbers
         //you will have to figure out how to copy the array elements from the passed array into a new
         //array and sort them and return the new sorted array
+        int[] myIntegers = getIntegers(5);
+        printArrays(myIntegers);
+
+        }
+    public static int[] getIntegers(int number){
+        System.out.println("Enter "+number+" integer values\r");
+        int[] values = new int[number];
+
+        for (int i =0;i<values.length;i++){
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
     }
+    public static void printArrays(int[]array){
+        for(int i =0;i<array.length;i++){
+            System.out.println("Element "+i+" typed values was "+array[i]);
+        }
+
+    }
+
+
+    public static void sortIntegers(int[] array){
+
+        for(int i=0;i<array.length;i++){
+            if(array[i]>(array[array.length - 1 - i])){
+                array[i] = array[i+0];
+                int arraySorte = array[i];
+            }
+        }
+
+    }
+
 
 }
